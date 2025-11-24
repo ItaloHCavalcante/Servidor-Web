93684ba8bdf087c1f0d448565538e22af4a73878
Italo Henrique <167244478+ItaloHCavalcante@users.noreply.github.com>
2025-11-23 22:08:25 -0300

    Refactor servidor.py for improved readability

--- Servidor_web/src/servidor.py
+++ Servidor_web/src/servidor.py
@@ -5,39 +5,38 @@ import sys
 portaServer = 6789
 
 # Criando o socket TCP
-serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket = socket(AF_INET, SOCK_STREAM) #AF_INET--> indica que será utilizado o IPv4
 
 
 serverSocket.bind(('', portaServer)) # Associa o socket com a porta definida em todas as interfaces de rede --> ('')
 serverSocket.listen(1) # Prepara o socket para aceitar conexões, o argumento(1) indica que suporta apenas 1 conexão pendente
 
 while True:  # Esperando por uma conexão
-
     print('Servidor pronto para ser utilizado...')
     connectionSocket, addr = serverSocket.accept() # Bloqueia o programa até que um socket tentar se conectar,
                                                    #e ele retornará um novo socket de conexão dedicado ao cliente (addr)
-
+    
     try:
         message = connectionSocket.recv(2024).decode() # Indica que o socket receberá até 2024 bytes do cliente,
-                                                       #em seguida irá decodificar-los (Ex:01010110 --> 'Hello Word')
+                                                       #em seguida irá decodificar-los (Ex: 01010110 --> 'Hello Word')
 
         # Analisará a requisição para obter o nome do arquivo
         filename = message.split()[1]  # O split dividirá o arquivo em partes (método, caminho, vesão)
 
-        #Abre o arquivo, o argumento indica que a '/' inicial será dispensada
+        # Abre o arquivo, o indice indica que a '/' inicial será dispensada
         f = open(filename[1:])
 
-        #Lê o conteúdo do arquivo
+        # Lê o conteúdo do arquivo
         outputdata = f.read()
 
-        #Envia uma linha de status HTTP 200 OK, e depois linhas em branco para indicar o fim do cabeçalho e inicio da mensagem
+        # Envia uma linha de status HTTP 200 OK, e depois linhas em branco para indicar o fim do cabeçalho e inicio da mensagem
         connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
 
         # Envia o conteúdo do arquivo ao cliente
         for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
+            connectionSocket.send(outputdata[i].encode()) # O índice indica que a cada looping ele enviará um caractere
         connectionSocket.send("\r\n".encode())
-        #Fecha a conexão com o cliente
+        # Encerra a conexão com o cliente
         connectionSocket.close()
 
     except IOError:
@@ -47,4 +46,5 @@ while True:  # Esperando por uma conexão
 
         # Encerra a conexão com o SOCKET do cliente
         connectionSocket.close()
+
 
